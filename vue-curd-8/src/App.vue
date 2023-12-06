c168ebf2c80d9f20744404018791acbaed6ce1bc
Maksud Bhuiyan <maksudbhuiyan595>
2023-12-07 03:03:41 +0600

    desigin

--- vue-curd-8/src/App.vue
+++ vue-curd-8/src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { RouterView } from 'vue-router';
 
-
-
-
 </script>
 
 <template>
-  
+  <div>
+    <div
+     class="text-4xl
+            text-center
+            font-bold
+            bg-green-600
+            p-3
+            ">
+      <h1>Vue 3 Crud Operatin</h1>
+    </div>
+  </div>
+
   <RouterView/>
 </template>
 <style>
--- vue-curd-8/src/components/student/List.vue
+++ vue-curd-8/src/components/student/List.vue
@@ -1,12 +1,50 @@
 <script setup>
+import { 
+      PencilIcon,
+      EyeIcon,
+      UserPlusIcon,
+      TrashIcon
+      } from '@heroicons/vue/24/solid'
 
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    List
-  </h1>
-</template>
-<style>
+      <div class="text-center
+            bg-orange-600
+            p-2
+            text-[#fff]
+            text-xl">
+            
+            Student Lists
+      </div>
 
-</style>
+      <div>
+            <table class="table-fixed ">
+                  <thead class="bg-slate-600
+                               text-white">
+                        <tr>
+                              <th>ID</th>
+                              <th>Name</th>
+                              <th>Email</th>
+                              <th>Action</th>
+                        </tr>
+                  </thead>
+                  <tbody>
+                        <tr>
+                              <td>1</td>
+                              <td>Maksud</td>
+                              <td>maksud@gmail.com</td>
+                              <td class="flex">
+                                   
+                                    <EyeIcon/>
+      
+                                  <PencilIcon/>
+                                  <TrashIcon/>
+                                    
+                              </td>
+                        </tr>
+                  </tbody>
+            </table>
+      </div>
+</template>
+<style></style>
